5db4b46cc7fa60d4ebbe22f915537aed1c4786a4
Gagancreates <gaganp000999@gmail.com>
2025-04-11 22:49:22 +0530

    done with the test

--- College Work/TestPrep/unit2.c
+++ College Work/TestPrep/unit2.c
@@ -287,38 +287,49 @@
 
 // selection sort
 
-#include<stdio.h>
-void selection(int arr[], int n){
-    for(int i=0; i<n-1;i++){
-        int min=i;
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[min]){
-                min=j;
-            }
-            int temp=arr[i];
-            arr[i]=arr[min];
-            arr[min]= temp;
-        }
-    }
+// #include<stdio.h>
+// void selection(int arr[], int n){
+//     for(int i=0; i<n-1;i++){
+//         int min=i;
+//         for(int j=i+1; j<n; j++){
+//             if(arr[j]<arr[min]){
+//                 min=j;
+//             }
+//             int temp=arr[i];
+//             arr[i]=arr[min];
+//             arr[min]= temp;
+//         }
+//     }
   
-    }
-    void printArray(int arr[], int n){
-        for(int i = 0; i < n; i++){
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-    }
-    int main(){
-        int arr[] = {64, 25, 12, 22, 11};
-        int n = sizeof(arr) / sizeof(arr[0]);
+//     }
+//     void printArray(int arr[], int n){
+//         for(int i = 0; i < n; i++){
+//             printf("%d ", arr[i]);
+//         }
+//         printf("\n");
+//     }
+//     int main(){
+//         int arr[] = {64, 25, 12, 22, 11};
+//         int n = sizeof(arr) / sizeof(arr[0]);
     
-        printf("Original array: ");
-        printArray(arr, n);
+//         printf("Original array: ");
+//         printArray(arr, n);
     
-        selectionsort(arr, n);
+//         selectionsort(arr, n);
     
-        printf("Sorted array: ");
-        printArray(arr, n);
+//         printf("Sorted array: ");
+//         printArray(arr, n);
     
-        return 0;
-    }
+//         return 0;
+//     }
+
+#include<stdio.h>
+int main()
+{	   
+    char a[] = "whatsapp" ;
+  char *b = "when" ;
+   int i;
+  for (i = 0 ; *b != '\0' ; i++,b++)
+  {	a[i] = *(b+i);   }
+  printf("%s",a);
+}
